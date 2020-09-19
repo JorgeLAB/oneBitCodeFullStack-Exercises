a21096651e3bbcd5cd3cadf1857190f73407ca4d
Jorge Borges <jorgetrabalho2016@gmail.com>
2020-09-19 20:47:53 -0300

    Javascript request: consultando outra api da Nasa - sentry.

---
+++ javascript_api/Exercicio_de_fetch/src/sentry.js
@@ -0,0 +1,9 @@
+export default class Sentry{
+	constructor(sentryId, name, minYearSentry, maxYearSentry){
+		this.sentryId = sentryId
+		this.name = name 
+		this.minYearSentry = minYearSentry
+		this.maxYearSentry = maxYearSentry
+	}
+
+}
