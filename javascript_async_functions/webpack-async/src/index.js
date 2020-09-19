41cbfb2988d967c24437a45f436022bdd618de3a
Jorge Borges <jorgetrabalho2016@gmail.com>
2020-09-19 14:43:49 -0300

    Javascript usando async function: Empregando await para substituir os then.

--- javascript_async_functions/webpack-async/src/index.js
+++ javascript_async_functions/webpack-async/src/index.js
@@ -36,9 +36,11 @@ function moveAndFire(x,y,z){
 // Podemos substituir as estruturas de Promise e colocar async functions no lugar.
 // Ainda empregando o then
 
+/*
+
 async function adjustPosition(x,y,z){
 	if(z > 9){
-		return Promise.reject("ângulo z inválido.");
+		return Promise.reject("ângulo z inválido.");  // tratamento de erro em uma async function
 	}
 	laserGun.currentPosition = [x,y,z];
 	return [x,y,z];
@@ -57,6 +59,39 @@ function moveAndFire(x,y,z){
 		console.log(`Começando a atirar nas coordenadas(${coord[0]}, ${coord[1]}, ${coord[2]})`);
 	}).catch(error => console.log(error));
 }
+
+*/
+
+
+// Substuindo os then por await temo o retorno diredo das promises.
+// Porém para o tratamento de error temos de empregar try{}catch{};
+
+
+async function adjustPosition(x,y,z){
+	if(z > 90){
+		return Promise.reject("ângulo z inválido.");  // tratamento de erro em uma async function
+	}
+	laserGun.currentPosition = [x,y,z];
+	return [x,y,z];
+}
+
+async function fire(x,y,z){
+	laserGun.firing = true;
+	return [x,y,z];
+}
+
+async function moveAndFire(x,y,z){
+	try{
+		let posicaoAjustada = await adjustPosition(x,y,z);
+		console.log(`Arma ajustada para as coordenadas(${posicaoAjustada[0]}, ${posicaoAjustada[1]}, ${posicaoAjustada[2]})`);
+	
+		let fireStatusPosition = await fire(x,y,z);
+		console.log(`Começando a atirar nas coordenadas(${fireStatusPosition[0]}, ${fireStatusPosition[1]}, ${fireStatusPosition[2]})`);
+	}catch(error){
+		console.log(`Deu: ${error}`);
+	}
+}
+
 
 
 moveAndFire(20,40,10);
